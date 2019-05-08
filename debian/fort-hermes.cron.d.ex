1b7f1036656cbb943aa392a4cebdb31202dc9459
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2019-05-08 16:50:37 +0200

    First debianization configuration

--- debian/fort-hermes.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the fort-hermes package
-#
-0 4	* * *	root	[ -x /usr/bin/fort-hermes_maintenance ] && /usr/bin/fort-hermes_maintenance
--- debian/shlibs.local.ex
+++
@@ -1,1 +0,0 @@
-libfort-hermes 0.1.0 fort-hermes (>> 0.1.0-0), fort-hermes (<< 0.1.0-99)
