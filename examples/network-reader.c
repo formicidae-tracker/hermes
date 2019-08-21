8dca5c8e22c3bf42de38a927c2c3000937de1d08
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2019-08-21 17:55:07 +0200

    Adds an example to read over the network

---
+++ examples/network-reader.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+
+#include "hermes.h"
+
+#include <stdio.h>
+
+
+int main( int argc, char ** argv) {
+	if (argc != 2 ) {
+		fprintf(stderr,"I need 1 argument\n");
+	}
+
+	fh_error_t * err = fh_error_create();
+	fh_context_t * ctx = fh_connect(argv[1],err);
+	if ( ctx == NULL ) {
+		fprintf(stderr,"Got error %d: %s\n",err->code,err->what);
+		return 1;
+	}
+
+	fh_frame_readout_t * ro = fh_frame_readout_create();
+	while(true) {
+		bool ok = fh_context_read(ctx,ro,err);
+		if ( ok == false ) {
+			if ( err->code == FH_END_OF_STREAM ) {
+				break;
+			}
+			fprintf(stderr,"Got error %d: %s\n",err->code,err->what);
+			return 1;
+		}
+		printf("Frame: %d nb ants: %d\n",fh_frame_readout_frame_id(ro),fh_frame_readout_ant_size(ro));
+	}
+	fh_frame_readout_destroy(ro);
+	fh_context_destroy(ctx);
+	fh_error_destroy(err);
+	return 0;
+}
