3a54b5c4514a233705f59036a485c4c051b6223d
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2021-09-02 15:54:22 +0200

    Rewrites network reader example

---
+++ src/python/example/hermes-network-reader.py
@@ -0,0 +1,19 @@
+"""
+Minimal example to read hermes messages from a host in python
+"""
+import argparse
+import py_fort_hermes
+
+parser = argparse.ArgumentParser(
+    description='Python hermes network reader, close connection with Ctrl+C')
+parser.add_argument('host', type=str, help='tracking host <hostname.local>')
+args = parser.parse_args()
+
+if __name__ == '__main__':
+    try:
+        with py_fort_hermes.OpenNetworkStream(args.host) as s:
+            for ro in s:
+                print("FrameID: %d, tags: %d" % (ro.frameID, len(ro.tags)))
+    except KeyboardInterrupt:
+        print("Closing connection after KeyboardInterrupt.")
+    print("Connection closed.")
