73b0e9e6920834824b5023c913b63880f786d3d6
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2021-09-02 15:58:20 +0200

    Rewrites liveplot example

--- src/python/example/liveplot.py
+++ src/python/example/liveplot.py
@@ -3,24 +3,10 @@ Plots data received from a tracking host in "real time"
 """
 import argparse
 import time
-import socket
-from google.protobuf.internal.decoder import _DecodeVarint32
-import Header_pb2
-import FrameReadout_pb2
+import py_fort_hermes
 from matplotlib import pyplot as plt
 import matplotlib
 matplotlib.use('TkAgg')
-
-
-def get_msg(sock):
-    data_size = bytearray()
-    for i in range(0, 100):
-        data_size += sock.recv(1)
-        if bytes([b'\x80'[0] & data_size[i]]) == b'\x00':
-            break
-    msg_len, new_pos = _DecodeVarint32(data_size, 0)
-    buf = sock.recv(msg_len)
-    return buf
 
 
 parser = argparse.ArgumentParser(
@@ -28,7 +14,6 @@ parser = argparse.ArgumentParser(
 parser.add_argument('host', type=str, help='tracking host <hostname.local>')
 args = parser.parse_args()
 
-port = 4002
 
 fig, ax = plt.subplots()
 plt.ion()
@@ -36,19 +21,12 @@ plt.ion()
 x_lim = [0.0, 0.0]
 y_lim = [0.0, 0.0]
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    try:
-        host = socket.gethostbyname(args.host)
-        s.connect((host, port))
-        print("Connected to {}".format(args.host))
-        msg = get_msg(s)
-        header = Header_pb2.Header()
-        header.ParseFromString(msg)
+
+try:
+    with py_fort_hermes.OpenNetworkStream(args.host) as s:
         # TODO: header.width/height fields seem to send zero
         while plt.fignum_exists(fig.number):
-            msg = get_msg(s)
-            ro = FrameReadout_pb2.FrameReadout()
-            ro.ParseFromString(msg)
+            msg = next(s)
             x = [t.x for t in ro.tags]
             y = [t.y for t in ro.tags]
             ax.clear()
@@ -65,10 +43,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 '%Y-%m-%d %H:%M:%S', time.localtime(ro.time.seconds))))
             plt.draw()
             plt.pause(0.001)
-        s.shutdown(socket.SHUT_RDWR)
-        s.close()
-        print("Connectoin closed")
-    except KeyboardInterrupt:
-        s.shutdown(socket.SHUT_RDWR)
-        s.close()
-        print("Connectoin closed")
+        print("Closing connection after end of figure")
+except KeyboardInterrupt:
+    print("Closing connection after KeyboardInterrupt")
+print("Connection closed")
